e3aa68a7441d58d3deaaf397119e2cfc8d1259f1
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-12-18 15:52:27 -0800

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,17 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+export default class CoordinatesButton extends Component{
+    getCoordinates = (event) => {
+        let x = event.clientX;
+        let y = event.clientY
+        let coors = [x, y];
+        this.props.onReceiveCoordinates(coors);
+    }
+
+    render(){
+        return(
+            <button onClick = {this.getCoordinates}/>
+        )
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,10 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+export default class DelayedButton extends Component{
+    render(){
+        return(
+            <button />
+        )
+    }
+}
